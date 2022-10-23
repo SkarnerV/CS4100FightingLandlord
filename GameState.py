2dbab3c689bcef8f95efbaee7b2395ffe369cdb3
Skarner <skarner@Skarners-MacBook-Pro.local>
2022-10-23 19:48:55 -0400

    first commit

---
+++ GameState.py
@@ -0,0 +1,122 @@
+class GameState:
+    def __init__(self, discarded, player,current):
+        self.discarded = discarded 
+        self.player = player
+        self.current = current
+        self.lastIndex = 0
+
+    
+    def toMove(self):
+        if len(self.discarded) == 0:
+            for i in range(0,len(self.player)):
+                if self.player[i].role == 'landlord':
+                    return  i
+        else: 
+            if self.lastIndex == len(self.player)-1:
+                return 0
+            else:
+                return self.lastIndex+1
+
+
+    # hand is sorted
+    # hand would be able to check types and weight
+    # hand: list of hand or list
+    def getActions(self):
+        
+        currentHand = self.player[self.toMove()].hand
+
+        actions = []
+        def getSingle():
+            collection = []
+            for i in currentHand:
+                collection.push([i])
+            return collection
+
+        def getDouble():
+            collection = []
+            for i in range(0,len(currentHand)-1):
+                if currentHand[i].value == currentHand[i+1].value:
+                    collection.push([currentHand[i],currentHand[i+1]])
+            return collection
+
+        def getTriple():
+            collection = []
+            for i in range(0,len(currentHand)-2):
+                if currentHand[i].value == currentHand[i+1].value and currentHand[i+1].value == currentHand[i+2].value:
+                    collection.push([currentHand[i],currentHand[i+1],currentHand[i+2]])
+                    for i in currentHand:
+                        if i != currentHand[i].value and i.suit != 'BJ' and i.suit != 'RJ':
+                            collection.push([currentHand[i],currentHand[i+1],currentHand[i+2]],i)
+            return collection
+
+        def getBomb():
+            collection = []
+            for i in range(0,len(currentHand)-3):
+                if currentHand[i].value == currentHand[i+1].value and currentHand[i+1].value == currentHand[i+2].value and currentHand[i+2].value == currentHand[i+3].value:
+                    collection.push([currentHand[i],currentHand[i+1],currentHand[i+2],currentHand[i+3]])
+            if currentHand[len(currentHand-1)].suit == 'RJ' and currentHand[len(currentHand-2)].suit == 'BJ':
+                    collection.push([currentHand[len(currentHand-2)],currentHand[len(currentHand-1)]])
+            return collection
+        
+        def getSequence():
+            collection = []
+            for i in range(0,len(currentHand)-4):
+                if currentHand[i].value == currentHand[i+1].value+1 and currentHand[i+1].value == currentHand[i+2].value+1 and currentHand[i+2].value == currentHand[i+3].value+1 and currentHand[i+3].value == currentHand[i+4].value+1:
+                    if currentHand[i+4].suit != 'RJ' and currentHand[i+4].suit != 'BJ':
+                        collection.push([currentHand[i],currentHand[i+1],currentHand[i+2],currentHand[i+3],currentHand[i+4]])
+            return collection
+
+
+        if(len(self.current) == 0):
+            actions.extend(getSingle())
+            actions.extend(getDouble())
+            actions.extend(getTriple())
+            actions.extend(getBomb())
+            actions.extend(getSequence())
+        
+        else:
+            currentCombo = self.current[len(self.current-1)]
+            if currentCombo.checkType() == 'single':
+                 actions.extend(getSingle())
+            if currentCombo.checkType() == 'double':    
+                actions.extend(getDouble())
+            if currentCombo.checkType() == 'triple':
+                actions.extend(getTriple())
+            if currentCombo.checkType() == 'bomb':
+                actions.extend(getBomb())
+            if currentCombo.checkType() == 'sequence':
+                actions.extend(getSequence())
+
+        return actions
+        
+
+    def isTerminal(self):
+        for i in self.player:
+            if len(i.hand) == 0:
+                return True
+        return False
+    
+    def getUtility(self):
+        if len(self.player[self.toMove()].hand) == 0:
+            return +1
+        
+        for i in self.player: 
+            if len(i.hand) == 0:
+                return -1
+            
+        return 0
+
+    def generateSuccessor(self,cards):
+
+        newPlayer = self.player.copy()
+        newPlayer[self.toMove()].hand.cards = [i for i in self.player[self.toMove()].hand.cards if i not in cards]
+        newRound = self.round.copy()
+        newRound.hand.cards.append(cards)
+        newDiscarded = self.discarded.copy()
+        newDiscarded.extend(cards)
+        
+
+
+        newState = GameState(newDiscarded,newPlayer,self.toMove)
+
+        return newState
